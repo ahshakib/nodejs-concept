c1fac5759cc09e78e2a4c59dc3a5160dee07d58b
ahshakib <ahshakib75@gmail.com>
2024-12-13 18:56:23 +0600

    zip sample.txt using stream

--- stream/index.js
+++ stream/index.js
@@ -1,7 +1,12 @@
 const express = require('express');
 const app = express();
 const fs = require("fs");
+const zlib = require("zlib")
 const port = 3030;
+
+fs.createReadStream('./sample.txt').pipe(
+    zlib.createGzip().pipe(fs.createWriteStream('./sample.zip'))
+)
 
 app.get('/', (req, res) => {
     const stream = fs.createReadStream('./sample.txt', 'utf-8');
